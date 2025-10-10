80740725b0adccab87c55e779735d83aeea994aa
Kidus Kibrom <kidusc941@gmail.com>
2025-10-10 12:13:18 +0300

    interest calculator working good

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+#calculate the simple interest on a given principal amount rate of interest and time
+#simple interest earned on an investment over a period of time
+
+principal = 1000
+rate = 0.05 # representing 5% annual rate interest 
+time = 3    # 3 years 
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest} ")
+
