118550dc15e7fcf1955c292e3e21b873f79682bd
Kidus Kibrom <kidusc941@gmail.com>
2025-10-10 13:24:19 +0300

    dam we getting old

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,13 @@
+#user input age calculator accepts user input and performs a simple arithmeticoperation to calculate the user age in a future year
+
+#accept user input inputs vaid integer value
+current_age = int(input("How old are you?"))
+
+
+#calcuate how old the user will be in the year 2025, current year = 2023, so add 27 years to the userss current afe
+
+
+age_2025 = current_age + 27
+
+print(f"In 2025, you will be {age_2025} years old ")
+
