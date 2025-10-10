f7f047d3189bad7f62f8128c46072dfa807f7a01
Kidus Kibrom <kidusc941@gmail.com>
2025-10-10 17:47:56 +0300

    creates a simple bank account class i need to come back to check it more

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,1 +1,21 @@
+# encapsulates banking operations use command line arguments to interact with instance of theis class
 
+class BankAccount:
+    def __init__(self, initail_balance = 0):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        print(f"Current Balance: {self.account_balance} ")
+        
