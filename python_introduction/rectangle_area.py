d9777dac5103daa410e1e9df9e5853dd05d0ac8b
Kidus Kibrom <kidusc941@gmail.com>
2025-10-10 12:28:15 +0300

    calcualte area of rectanfle with simple varibles

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,10 @@
+# calculatinf the arean of a rectangle this is found by multiplying its length by width
+
+length = 10
+width = 5
+
+area = length * width 
+
+print(f"The area of the rectangle is: {area} ")
+
+
