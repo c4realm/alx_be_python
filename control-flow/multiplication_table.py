c1cd90210e3c09a62c7ec7589d0760832cf83c4d
Kidus Kibrom <kidusc941@gmail.com>
2025-12-07 14:07:36 +0300

    correction added

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 #understanding the question, use for loop to generate and print the multiplication table for a given number, enter a number, then use a for loop to print multiplication table for that number from 1 to 10.
 
-number = int(input("Enter a number to see its multipication table: "))
+number = int(input("Enter a number to see its multipication table:"))
 
 for i in range(1, 11):
     product = number * i
