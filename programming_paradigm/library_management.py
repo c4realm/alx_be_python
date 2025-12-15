905d33a7374fa2509f135c70bd64e552ac3faca8
root <kidusc941@gmail.com>
2025-12-15 05:19:08 -0800

    added system at tracks books in a library

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,50 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+        else:
+            print(f"'{self.title}' is already checked out.")
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+        else:
+            print(f"'{self.title}' is not currently checked out.")
+
+    def is_available(self):
+        return not self._is_checked_out
+
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                book.check_out()
+                return
+        print(f"Book '{title}' not found in the library.")
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                book.return_book()
+                return
+        print(f"Book '{title}' not found in the library.")
+
+    def list_available_books(self):
+        available_books = [book for book in self._books if book.is_available()]
+        if not available_books:
+            print("No books are currently available.")
+        else:
+            for book in available_books:
+                print(f"{book.title} by {book.author}")
